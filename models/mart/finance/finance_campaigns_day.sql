bc7a3ed0bdf74580d94f3b0fc010c90a74297afb
Arunvir <arunviratwal@gmail.com>
2024-09-11 21:31:13 +0000

    updated mart code for campaigns sorted by date

--- models/mart/finance/finance_campaigns_day.sql
+++ models/mart/finance/finance_campaigns_day.sql
@@ -1,10 +1,20 @@
-SELECT 
+
+SELECT
 date_date,
-ROUND(SUM(operational_margin - ads_cost),2) AS ads_margin,
-ROUND(SUM(average_basket),2) AS average_basket,
-ROUND(SUM(operational_margin),2) AS operational_margin
-FROM {{ ref("int_campaigns_day") }}
-LEFT JOIN `{{ ref("finance_days") }}
+operational_margin - ads_cost AS ads_margin,
+ROUND(average_basket,2) AS average_basket,
+operational_margin,
+ads_cost,
+ads_impression,
+ads_clicks,
+quantity,
+revenue,
+purchase_cost,
+margin,
+shipping_fee,
+logcost,
+ship_cost
+FROM {{ ref('int_campaigns_day') }}
+FULL OUTER JOIN {{ ref('finance_days') }}
 USING (date_date)
-GROUP BY date_date
 ORDER BY date_date DESC
